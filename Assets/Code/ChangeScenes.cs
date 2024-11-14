261acf822ec6ca71fed9015174e81ecbdedbf8bf
lilvishi <sam.vaishnavi@gmail.com>
2024-11-14 15:59:59 -0800

    fixish

--- Assets/Code/ChangeScenes.cs
+++ Assets/Code/ChangeScenes.cs
@@ -4,14 +4,11 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 
-public PlayerController playerController;
-
 public class ChangeScenes : MonoBehaviour
 {
   public void GoToSceneTwo()
   {
     SceneManager.LoadScene("Underground");
-    playerController.AddListener(UnfreezeMovement);
 
   }
 }
