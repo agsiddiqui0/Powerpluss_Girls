59e6def5abf93edde656ae6eccc4663fd74da320
lilvishi <sam.vaishnavi@gmail.com>
2024-11-14 16:14:44 -0800

    fixed player movement issues & got rid of sprint

--- Assets/Code/PlayerController.cs
+++ Assets/Code/PlayerController.cs
@@ -9,7 +9,7 @@ public class PlayerController : MonoBehaviour
     private SpriteRenderer sr;
     private Vector2 movementVector;
     private Rigidbody2D rb;
-    int speed = 4;
+    const int speed = 4;
     public bool movementEnabled = true;
     int orb = 0;
     //int animalpoints = 0;
@@ -33,8 +33,8 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-         if (movementEnabled) {
-
+         if (movementEnabled) 
+         {
             rb.velocity = new Vector2(speed * movementVector.x, speed * movementVector.y);
 
             movementVector.x = Input.GetAxisRaw("Horizontal");
@@ -75,7 +75,6 @@ public class PlayerController : MonoBehaviour
     public void FreezeMovement()
     {
         rb.velocity = Vector2.zero;
-        
         movementEnabled = false;
     }
 
@@ -84,17 +83,6 @@ public class PlayerController : MonoBehaviour
     {
         movementEnabled = true;
 
-    }
-
-
-    void OnSprintPress (InputValue value)
-    {
-        speed = 7;
-    }
-
-    void OnSprintRelease (InputValue value)
-    {
-        speed = 4;
     }
 
     private void OnTriggerEnter2D (Collider2D collision)
