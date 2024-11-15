ea074f9166ac3bc5e6a6deccd4d1ab51e6477a08
eladefneerkan <edefneerkan@g.ucla.edu>
2024-11-14 17:57:23 -0800

    player

--- Assets/Code/PlayerController.cs
+++ Assets/Code/PlayerController.cs
@@ -98,7 +98,7 @@ public class PlayerController : MonoBehaviour
         if (collision.gameObject.CompareTag("Suit_Animal"))
         {
             animalpoints++;
-            Debug.Log("Animal points: " + animalpoints);
+            Debug.Log("Animal points: ");
         }
 
        
