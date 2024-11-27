71df9449a3fd0a1f59960f88b633575d7dd3c9bc
eladefneerkan <edefneerkan@g.ucla.edu>
2024-11-27 12:29:58 -0800

    Stuff

--- Assets/Code/ChangeScene.cs
+++ Assets/Code/ChangeScene.cs
@@ -5,7 +5,6 @@ using UnityEngine.SceneManagement;
 
 public class ChangeSceneTrigger : MonoBehaviour
 {
-
     private void OnTriggerEnter2D(Collider2D collision) {
         SceneManager.LoadScene("Middle");
     }
