39bc2451d57ed02220bd95b5bca7c3c885588abc
lilvishi <sam.vaishnavi@gmail.com>
2024-11-29 22:41:00 -0800

    fixed a bunch of code issues

--- Assets/Code/ChangeScene.cs
+++ Assets/Code/ChangeScene.cs
@@ -3,10 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+
+
 public class ChangeSceneTrigger : MonoBehaviour
 {
+    [SerializeField] string sceneName;
+
     private void OnTriggerEnter2D(Collider2D collision) {
-        SceneManager.LoadScene("Middle");
+        SceneManager.LoadScene(sceneName);
     }
 
 }
