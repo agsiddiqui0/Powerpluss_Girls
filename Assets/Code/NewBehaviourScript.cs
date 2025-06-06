89fa191b60e1230165d0d8cafe3bc41bd0e4fd86
agsiddiqui0 <anamgsiddiqui@gmail.com>
2025-06-05 17:12:05 -0700

    ljasdfljsf

--- Assets/Code/NewBehaviourScript.cs
+++ Assets/Code/NewBehaviourScript.cs
@@ -1,18 +1,79 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
 
-public class NewBehaviourScript : MonoBehaviour
+public class TeleportTrigger : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject Player;
+    public Transform teleportTarget;
+    public CanvasGroup blackPanel;
+    public float fadeDuration = 0.5f;
+
+    private bool canTrigger = false;
+    private bool isFading = false;
+
+    void OnTriggerEnter(Collider other)
     {
-        
+        if (canTrigger && other.gameObject == Player && !isFading)
+        {
+            StartCoroutine(HandleTeleport());
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void EnableTriggerAfterDialogue()
     {
-        
+        canTrigger = true;
+    }
+
+    IEnumerator HandleTeleport()
+    {
+        isFading = true;
+
+        // Freeze time
+        Time.timeScale = 0f;
+
+        // Fade in black panel
+        float t = 0;
+        while (t < fadeDuration)
+        {
+            t += Time.unscaledDeltaTime;
+            blackPanel.alpha = Mathf.Lerp(0, 1, t / fadeDuration);
+            yield return null;
+        }
+
+        blackPanel.alpha = 1;
+
+        // Move player
+        Player.transform.position = teleportTarget.position;
+
+        // Fade out player (optional)
+        SpriteRenderer sr = Player.GetComponent<SpriteRenderer>();
+        if (sr != null)
+        {
+            t = 0;
+            Color original = sr.color;
+            while (t < fadeDuration)
+            {
+                t += Time.unscaledDeltaTime;
+                sr.color = new Color(original.r, original.g, original.b, Mathf.Lerp(1, 0, t / fadeDuration));
+                yield return null;
+            }
+        }
+
+        // Unfreeze time
+        Time.timeScale = 1f;
+
+        yield return new WaitForSeconds(0.5f);
+
+        // Fade panel back to clear if needed
+        t = 0;
+        while (t < fadeDuration)
+        {
+            t += Time.deltaTime;
+            blackPanel.alpha = Mathf.Lerp(1, 0, t / fadeDuration);
+            yield return null;
+        }
+
+        blackPanel.alpha = 0;
     }
 }
